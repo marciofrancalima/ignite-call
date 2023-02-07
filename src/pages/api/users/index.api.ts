ee4766ffb01153f82b2c0a5e087b001079290585
Márcio França Lima <marciofrancalima@hotmail.com>
2023-02-07 11:58:02 -0300

    feat: persist user data in database

---
+++ src/pages/api/users/index.api.ts
@@ -0,0 +1,19 @@
+import { prisma } from '@/src/lib/prisma'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const { username, name } = req.body
+
+  const user = await prisma.user.create({
+    data: { username, name },
+  })
+
+  return res.status(201).json(user)
+}
