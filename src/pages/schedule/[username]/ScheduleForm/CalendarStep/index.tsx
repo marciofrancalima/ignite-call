10bec93694f1e960ab39e08d9d1b90efc824c25a
Márcio França Lima <marciofrancalima@hotmail.com>
2023-02-10 18:21:56 -0300

    feat: add schedule page for user

---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,10 @@
+import { Calendar } from '../../../../../components/Calendar/intex'
+import { Container } from './style'
+
+export function CalendarStep() {
+  return (
+    <Container>
+      <Calendar />
+    </Container>
+  )
+}
